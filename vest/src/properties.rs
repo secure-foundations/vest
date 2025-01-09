e589700a613f65fc49444d7e993cb914f746fcc1
y1ca1 <caiyeee@outlook.com>
2025-01-09 16:55:20 -0500

    Some notes about recursive formats

--- vest/src/properties.rs
+++ vest/src/properties.rs
@@ -5,8 +5,10 @@ use vstd::*;
 
 verus! {
 
+/// The parse result of a combinator.
 pub type PResult<T, E> = Result<(usize, T), E>;
 
+/// The serialize result of a combinator.
 pub type SResult<T, E> = Result<T, E>;
 
 /// Specification for parser and serializer [`Combinator`]s. All Vest combinators must implement this
@@ -271,6 +273,75 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
     }
 }
 
+impl<C: SpecCombinator> SpecCombinator for Box<C> {
+    type Type = C::Type;
+
+    open spec fn spec_parse(&self, s: Seq<u8>) -> Result<(usize, Self::Type), ()> {
+        (**self).spec_parse(s)
+    }
+
+    open spec fn spec_serialize(&self, v: Self::Type) -> Result<Seq<u8>, ()> {
+        (**self).spec_serialize(v)
+    }
+
+    proof fn spec_parse_wf(&self, s: Seq<u8>) {
+        (**self).spec_parse_wf(s)
+    }
+}
+
+impl<C: SecureSpecCombinator> SecureSpecCombinator for Box<C> {
+    open spec fn is_prefix_secure() -> bool {
+        C::is_prefix_secure()
+    }
+
+    proof fn theorem_serialize_parse_roundtrip(&self, v: Self::Type) {
+        (**self).theorem_serialize_parse_roundtrip(v)
+    }
+
+    proof fn theorem_parse_serialize_roundtrip(&self, buf: Seq<u8>) {
+        (**self).theorem_parse_serialize_roundtrip(buf)
+    }
+
+    proof fn lemma_prefix_secure(&self, s1: Seq<u8>, s2: Seq<u8>) {
+        (**self).lemma_prefix_secure(s1, s2)
+    }
+}
+
+impl<I, O, C: Combinator<I, O>> Combinator<I, O> for Box<C> where
+    I: VestInput,
+    O: VestOutput<I>,
+    C::V: SecureSpecCombinator<Type = <C::Type as View>::V>,
+ {
+    type Type = C::Type;
+
+    open spec fn spec_length(&self) -> Option<usize> {
+        (**self).spec_length()
+    }
+
+    fn length(&self) -> Option<usize> {
+        (**self).length()
+    }
+
+    open spec fn parse_requires(&self) -> bool {
+        (**self).parse_requires()
+    }
+
+    fn parse(&self, s: I) -> (res: Result<(usize, Self::Type), ParseError>) {
+        (**self).parse(s)
+    }
+
+    open spec fn serialize_requires(&self) -> bool {
+        (**self).serialize_requires()
+    }
+
+    fn serialize(&self, v: Self::Type, data: &mut O, pos: usize) -> (res: Result<
+        usize,
+        SerializeError,
+    >) {
+        (**self).serialize(v, data, pos)
+    }
+}
+
 // The following is an attempt to support `Fn`s as combinators.
 // I started implementing it because Verus doesn't support existential types (impl Trait) yet,
 // which is required to be able to put `Depend` combinator in the function's return type,
@@ -288,16 +359,16 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 // type parameters instead for both parsing and serialization, but that would require another
 // entire-codebase refactoring, which I think is not worth it at this point.
 //
-// #[verifier::reject_recursive_types(SpecResult)]
-// pub struct SpecCombinatorFn<SpecResult, const Prefix: u8> {
-//     pub parse: spec_fn(Seq<u8>) -> Result<(usize, SpecResult), ()>,
-//     pub serialize: spec_fn(SpecResult) -> Result<Seq<u8>, ()>,
+// #[verifier::reject_recursive_types(Type)]
+// pub struct SpecCombinatorFn<Type, const Prefix: u8> {
+//     pub parse: spec_fn(Seq<u8>) -> PResult<Type, ()>,
+//     pub serialize: spec_fn(Type) -> SResult<Seq<u8>, ()>,
 // }
 //
-// impl<SpecResult, const Prefix: u8> SpecCombinatorFn<SpecResult, Prefix> {
+// impl<Type, const Prefix: u8> SpecCombinatorFn<Type, Prefix> {
 //     pub open spec fn new(
-//         parse: spec_fn(Seq<u8>) -> Result<(usize, SpecResult), ()>,
-//         serialize: spec_fn(SpecResult) -> Result<Seq<u8>, ()>,
+//         parse: spec_fn(Seq<u8>) -> PResult<Type, ()>,
+//         serialize: spec_fn(Type) -> SResult<Seq<u8>, ()>,
 //     ) -> (o: Self)
 //         recommends
 //             forall|v| Self::theorem_serialize_parse_roundtrip(parse, serialize, v),
@@ -308,44 +379,39 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //     }
 //
 //     pub open spec fn theorem_serialize_parse_roundtrip(
-//         parse: spec_fn(Seq<u8>) -> Result<(usize, SpecResult), ()>,
-//         serialize: spec_fn(SpecResult) -> Result<Seq<u8>, ()>,
-//         v: SpecResult,
+//         parse: spec_fn(Seq<u8>) -> PResult<Type, ()>,
+//         serialize: spec_fn(Type) -> SResult<Seq<u8>, ()>,
+//         v: Type,
 //     ) -> bool {
-//         serialize(v) is Ok ==> parse(serialize(v).unwrap()) == Ok::<_, ()>(
-//             (serialize(v).unwrap().len() as usize, v),
-//         )
+//         serialize(v) matches Ok(b) ==> parse(b) == Ok::<_, ()>((b.len() as usize, v))
 //     }
 //
 //     pub open spec fn theorem_parse_serialize_roundtrip(
-//         parse: spec_fn(Seq<u8>) -> Result<(usize, SpecResult), ()>,
-//         serialize: spec_fn(SpecResult) -> Result<Seq<u8>, ()>,
+//         parse: spec_fn(Seq<u8>) -> Result<(usize, Type), ()>,
+//         serialize: spec_fn(Type) -> Result<Seq<u8>, ()>,
 //         buf: Seq<u8>,
 //     ) -> bool {
-//         buf.len() <= usize::MAX ==> parse(buf) is Ok ==> serialize(parse(buf).unwrap().1) == Ok::<
-//             _,
-//             (),
-//         >(buf.subrange(0, parse(buf).unwrap().0 as int))
+//         buf.len() <= usize::MAX ==> ( parse(buf) matches Ok((n, v)) ==> serialize(v) == Ok::<
+//         _,
+//         (),
+//         >(buf.take(n as int)) )
 //     }
 //
 //     pub open spec fn lemma_prefix_secure(
-//         parse: spec_fn(Seq<u8>) -> Result<(usize, SpecResult), ()>,
+//         parse: spec_fn(Seq<u8>) -> Result<(usize, Type), ()>,
 //         s1: Seq<u8>,
 //         s2: Seq<u8>,
 //     ) -> bool {
-//         if s1.len() + s2.len() <= usize::MAX {
-//             (Prefix == 1) ==> parse(s1) is Ok ==> parse(s1.add(s2)) == parse(s1)
-//         } else {
-//             true
-//         }
+//         s1.len() + s2.len() <= usize::MAX && (Prefix == 1) ==>
+//         parse(s1) is Ok ==> parse(s1 + s2) == parse(s1)
 //     }
 // }
 //
 //
-// impl<SpecResult, const Prefix: u8> SpecCombinator for SpecCombinatorFn<SpecResult, Prefix> {
-//     type SpecResult = SpecResult;
+// impl<Type, const Prefix: u8> SpecCombinator for SpecCombinatorFn<Type, Prefix> {
+//     type Type = Type;
 //
-//     open spec fn spec_parse(&self, s: Seq<u8>) -> Result<(usize, Self::SpecResult), ()> {
+//     open spec fn spec_parse(&self, s: Seq<u8>) -> Result<(usize, Self::Type), ()> {
 //         if let Ok((n, v)) = (self.parse)(s) {
 //             if n <= s.len() {
 //                 Ok((n, v))
@@ -357,7 +423,7 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //         }
 //     }
 //
-//     open spec fn spec_serialize(&self, v: Self::SpecResult) -> Result<Seq<u8>, ()> {
+//     open spec fn spec_serialize(&self, v: Self::Type) -> Result<Seq<u8>, ()> {
 //         (self.serialize)(v)
 //     }
 //
@@ -365,12 +431,12 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //     }
 // }
 //
-// impl<SpecResult, const Prefix: u8> SecureSpecCombinator for SpecCombinatorFn<SpecResult, Prefix> {
-//     open spec fn spec_is_prefix_secure() -> bool {
+// impl<Type, const Prefix: u8> SecureSpecCombinator for SpecCombinatorFn<Type, Prefix> {
+//     open spec fn is_prefix_secure() -> bool {
 //         Prefix == 1
 //     }
 //
-//     proof fn theorem_serialize_parse_roundtrip(&self, v: Self::SpecResult) {
+//     proof fn theorem_serialize_parse_roundtrip(&self, v: Self::Type) {
 //         assume(Self::theorem_serialize_parse_roundtrip(self.parse, self.serialize, v));
 //     }
 //
@@ -383,16 +449,18 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //     }
 // }
 //
-// pub struct CombinatorFn<'a, 'b, R, P, S, const Prefix: u8> where
+// pub struct CombinatorFn<I, O, R, P, S, const Prefix: u8> where
+//     I: VestInput,
+//     O: VestOutput<I>,
 //     R: View,
-//     P: Fn(&'a [u8]) -> Result<(usize, R), ()>,
-//     S: Fn(R, &'b mut Vec<u8>, usize) -> Result<usize, ()>,
+//     P: Continuation<I, Output = PResult<R, ParseError>>,
+//     S: for<'a> Continuation<(R, &'a mut O, usize), Output = SResult<usize, SerializeError>>,
 // {
 //     pub parse: P,
 //     pub serialize: S,
-//     pub spec_parse: Ghost<spec_fn(Seq<u8>) -> Result<(usize, R::V), ()>>,
-//     pub spec_serialize: Ghost<spec_fn(R::V) -> Result<Seq<u8>, ()>>,
-//     pub _phantom: std::marker::PhantomData<&'b &'a R>,
+//     pub spec_parse: Ghost<spec_fn(Seq<u8>) -> PResult<R::V, ()>>,
+//     pub spec_serialize: Ghost<spec_fn(R::V) -> SResult<Seq<u8>, ()>>,
+//     phantom: std::marker::PhantomData<(I, O)>,
 // }
 //
 // impl<'a, 'b, R, P, S, const Prefix: u8> View for CombinatorFn<'a, 'b, R, P, S, Prefix> where
@@ -416,7 +484,7 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //     R: View,
 //     P: Fn(&'a [u8]) -> Result<(usize, R), ()>,
 //     S: Fn(R, &'b mut Vec<u8>, usize) -> Result<usize, ()>,
-//     Self::V: SecureSpecCombinator<SpecResult = R::V>,
+//     Self::V: SecureSpecCombinator<Type = R::V>,
 //  {
 //     type Result<'c> = R;
 //
@@ -443,3 +511,313 @@ impl<I, O, C: Combinator<I, O>> Combinator<I, O> for &C where
 //     }
 // }
 } // verus!
+///////// Separating the parsing and serializing functions
+///////// Unsuccesful due to conflicting trait impls and Verus limitations (&mut support)
+// pub trait Parser<I, O>
+// where
+//     I: VestInput,
+// {
+//     type Type;
+//
+//     fn parse_fn(&self, s: I) -> PResult<Self::Type, ParseError>;
+// }
+//
+// pub trait Serializer<I, O>
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+// {
+//     type Type;
+//
+//     fn serialize_fn(
+//         &self,
+//         v: Self::Type,
+//         data: &mut O,
+//         pos: usize,
+//     ) -> SResult<usize, SerializeError>;
+// }
+//
+// impl<I, O, Type, F> Parser<I, O> for F
+// where
+//     I: VestInput,
+//     F: Fn(I) -> PResult<Type, ParseError>,
+// {
+//     type Type = Type;
+//
+//     fn parse_fn(&self, s: I) -> PResult<Self::Type, ParseError> {
+//         self(s)
+//     }
+// }
+//
+// impl<I, O, Fst, Snd> Parser<I, O> for (Fst, Snd)
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     Fst: Combinator<I, O>,
+//     Snd: Combinator<I, O>,
+//     Fst::V: SecureSpecCombinator<Type = <Fst::Type as View>::V>,
+//     Snd::V: SecureSpecCombinator<Type = <Snd::Type as View>::V>,
+// {
+//     type Type = (Fst::Type, Snd::Type);
+//
+//     fn parse_fn(&self, s: I) -> PResult<Self::Type, ParseError> {
+//         (&self.0, &self.1).parse(s)
+//     }
+// }
+//
+// impl<I: VestPublicInput, O: VestPublicOutput<I>> Parser<I, O> for crate::regular::uints::U8 {
+//     type Type = u8;
+//
+//     fn parse_fn(&self, s: I) -> PResult<Self::Type, ParseError> {
+//         <_ as Combinator<I, O>>::parse(self, s)
+//     }
+// }
+//
+// fn parse_pair_of_u8<I, O>(s: I) -> PResult<(u8, u8), ParseError>
+// where
+//     I: VestPublicInput,
+//     O: VestPublicOutput<I>,
+// {
+//     <_ as Parser<I, O>>::parse_fn(&(crate::regular::uints::U8, crate::regular::uints::U8), s)
+// }
+//
+// fn test<I, O, P: Parser<I, O>>(p: P, s: I) -> PResult<P::Type, ParseError>
+// where
+//     I: VestPublicInput,
+// {
+//     p.parse_fn(s)
+// }
+//
+// fn test2() {
+//     let s = Vec::new();
+//     let r = test::<_, Vec<u8>, _>(parse_pair_of_u8::<&[u8], Vec<u8>>, s.as_slice());
+// }
+// fn parse_pair<I, O, Fst, Snd>(
+//     fst: Fst,
+//     snd: Snd,
+//     s: I,
+// ) -> PResult<(Fst::Type, Snd::Type), ParseError>
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     Fst: Parser<I, O>,
+//     Snd: Parser<I, O>,
+// {
+//     // (fst, snd).parse(s)
+// }
+// impl<I, O, C: Combinator<I, O>> Parser<I, O> for C
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     C::V: SecureSpecCombinator<Type = <C::Type as View>::V>,
+// {
+//     type Type = C::Type;
+//
+//     fn parse_fn(&self, s: I) -> PResult<Self::Type, ParseError> {
+//         self.parse(s)
+//     }
+// }
+// impl<I, O, C: Combinator<I, O>> Serializer<I, O> for C
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     C::V: SecureSpecCombinator<Type = <C::Type as View>::V>,
+// {
+//     type Type = C::Type;
+//
+//     fn serialize_fn(
+//         &self,
+//         v: Self::Type,
+//         data: &mut O,
+//         pos: usize,
+//     ) -> SResult<usize, SerializeError> {
+//         self.serialize(v, data, pos)
+//     }
+// }
+// fn parse_pair<I, O, Fst, Snd>(
+//     fst: &Fst,
+//     snd: &Snd,
+//     s: I,
+// ) -> PResult<(Fst::Type, Snd::Type), ParseError>
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     Fst: Parser<I, Type = Fst::Type>,
+//     Snd: Parser<I, Type = Snd::Type>,
+// {
+//     let (n, v1) = fst.parse(s.clone())?;
+//     let s_ = s.subrange(n, s.len());
+//     let (m, v2) = snd.parse(s_)?;
+//     if let Some(nm) = n.checked_add(m) {
+//         Ok((nm, (v1, v2)))
+//     } else {
+//         Err(ParseError::SizeOverflow)
+//     }
+// }
+///////// "Lazy" combinators (`dyn` not supported by Verus yet) to support recursive formats
+// impl<C: View> View for dyn crate::regular::depend::Continuation<(), Output = C> {
+//     type V = C::V;
+//
+//     // spec fn view(&self) -> Self::V;
+// }
+// impl<I, O, C: Combinator<I, O>> Combinator<I, O>
+//     for Box<dyn crate::regular::depend::Continuation<(), Output = C>>
+// where
+//     I: VestInput,
+//     O: VestOutput<I>,
+//     C::V: SecureSpecCombinator<Type = <C::Type as View>::V>,
+// {
+//     type Type = Box<C::Type>;
+//
+//     fn length(&self) -> Option<usize> {
+//         None
+//     }
+//
+//     fn parse(&self, s: I) -> Result<(usize, Self::Type), ParseError> {
+//         match self.apply(()).parse(s) {
+//             Ok((n, v)) => Ok((n, Box::new(v))),
+//             Err(e) => Err(e),
+//         }
+//     }
+//
+//     fn serialize(&self, v: Self::Type, data: &mut O, pos: usize) -> Result<usize, SerializeError> {
+//         self.apply(()).serialize(*v, data, pos)
+//     }
+// }
+//////// The following works, but currently we cannot verify it due to Verus limitations
+// pub const INSTR_BASE: u8 = 0;
+// pub const AUXBLOCK_BEGIN: u8 = 1;
+// pub const AUXBLOCK_END: u8 = 11;
+//
+// #[derive(Debug)]
+// struct InstrFmt(Either<u8, Box<AuxBlockFmt>>);
+// #[derive(Debug)]
+// struct AuxBlockFmt((u8, (RepeatResult<Box<InstrFmt>>, u8)));
+//
+// impl vstd::view::View for InstrFmt {
+//     type V = Self;
+// }
+// impl vstd::view::View for AuxBlockFmt {
+//     type V = Self;
+// }
+//
+// struct InstrCom(
+//     pub OrdChoice<Refined<U8, TagPred<u8>>, Box<dyn Continuation<(), Output = AuxBlockCom>>>,
+// );
+// struct AuxBlockCom(
+//     pub  (
+//         Refined<U8, TagPred<u8>>,
+//         (
+//             Star<Box<dyn Continuation<(), Output = InstrCom>>>,
+//             Refined<U8, TagPred<u8>>,
+//         ),
+//     ),
+// );
+// impl vstd::view::View for InstrCom {
+//     type V = Self;
+// }
+// impl vstd::view::View for AuxBlockCom {
+//     type V = Self;
+// }
+// impl SpecCombinator for InstrCom {
+//     type Type = InstrFmt;
+// }
+// impl SecureSpecCombinator for InstrCom {}
+// impl SpecCombinator for AuxBlockCom {
+//     type Type = AuxBlockFmt;
+// }
+// impl SecureSpecCombinator for AuxBlockCom {}
+//
+// impl DisjointFrom<Refined<U8, TagPred<u8>>> for AuxBlockCom {}
+//
+// impl<'a> Combinator<&'a [u8], Vec<u8>> for InstrCom {
+//     type Type = InstrFmt;
+//     fn length(&self) -> Option<usize> {
+//         <_ as Combinator<&[u8], Vec<u8>>>::length(&self.0)
+//     }
+//     fn parse(&self, s: &'a [u8]) -> Result<(usize, Self::Type), ParseError> {
+//         match <_ as Combinator<&[u8], Vec<u8>>>::parse(&self.0, s) {
+//             Ok((n, Either::Left(v))) => Ok((n, InstrFmt(Either::Left(v)))),
+//             Ok((n, Either::Right(v))) => Ok((n, InstrFmt(Either::Right(v)))),
+//             Err(e) => Err(e),
+//         }
+//     }
+//     fn serialize(
+//         &self,
+//         v: Self::Type,
+//         data: &mut Vec<u8>,
+//         pos: usize,
+//     ) -> Result<usize, SerializeError> {
+//         <_ as Combinator<&[u8], Vec<u8>>>::serialize(&self.0, v.0, data, pos)
+//     }
+// }
+//
+// impl<'a> Combinator<&'a [u8], Vec<u8>> for AuxBlockCom {
+//     type Type = AuxBlockFmt;
+//     fn length(&self) -> Option<usize> {
+//         <_ as Combinator<&[u8], Vec<u8>>>::length(&self.0)
+//     }
+//     fn parse(&self, s: &'a [u8]) -> Result<(usize, Self::Type), ParseError> {
+//         match <_ as Combinator<&[u8], Vec<u8>>>::parse(&self.0, s) {
+//             Ok((n, (a, (b, c)))) => Ok((n, AuxBlockFmt((a, (b, c))))),
+//             Err(e) => Err(e),
+//         }
+//     }
+//     fn serialize(
+//         &self,
+//         v: Self::Type,
+//         data: &mut Vec<u8>,
+//         pos: usize,
+//     ) -> Result<usize, SerializeError> {
+//         <_ as Combinator<&[u8], Vec<u8>>>::serialize(&self.0, v.0, data, pos)
+//     }
+// }
+//
+// struct AuxBlockCont;
+// struct InstrCont;
+//
+// impl Continuation<()> for AuxBlockCont {
+//     type Output = AuxBlockCom;
+//
+//     fn apply(&self, i: ()) -> Self::Output {
+//         AuxBlockCom((
+//             Refined {
+//                 inner: U8,
+//                 predicate: TagPred(AUXBLOCK_BEGIN),
+//             },
+//             (
+//                 Star(Box::new(InstrCont)),
+//                 Refined {
+//                     inner: U8,
+//                     predicate: TagPred(AUXBLOCK_END),
+//                 },
+//             ),
+//         ))
+//     }
+// }
+//
+// impl Continuation<()> for InstrCont {
+//     type Output = InstrCom;
+//
+//     fn apply(&self, i: ()) -> Self::Output {
+//         InstrCom(OrdChoice(
+//             Refined {
+//                 inner: U8,
+//                 predicate: TagPred(INSTR_BASE),
+//             },
+//             Box::new(AuxBlockCont),
+//         ))
+//     }
+// }
+//
+// fn test() {
+//     // let buf = vec![0x00];
+//     let buf = vec![0x01, 0, 0, 0x01, 0, 0, 0, 0x0B, 0, 0x0B];
+//     let aux_block = AuxBlockCont.apply(());
+//     let instr = InstrCont.apply(());
+//     let (consumed, parsed) = instr.parse(&buf).unwrap_or_else(|e| {
+//         panic!("Failed to parse: {}", e);
+//     });
+//     println!("consumed: {}", consumed);
+//     println!("parsed: {:?}", parsed);
+// }
