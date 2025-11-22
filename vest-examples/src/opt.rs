8a65cf24fd36958ff6c476674cda8e62899a1df0
y1ca1 <caiyeee@outlook.com>
2025-11-22 15:04:16 -0500

    Fix vest-examples

--- vest-examples/src/opt.rs
+++ vest-examples/src/opt.rs
@@ -1,10 +1,11 @@
 use crate::my_vec;
 use vest_lib::properties::*;
 use vest_lib::regular::bytes;
+use vest_lib::regular::end::*;
 use vest_lib::regular::repetition::RepeatN;
 use vest_lib::regular::tag::*;
 use vest_lib::regular::uints::*;
-use vest_lib::regular::variant::{Choice, Opt};
+use vest_lib::regular::variant::{Choice, Opt, OptThen};
 use vstd::prelude::*;
 
 verus! {
@@ -19,6 +20,23 @@ exec fn parse_opt(x: usize) -> Result<(), Error>
     let mut data1 = my_vec![1u8, 0x40u8, 0xE2u8, 0x01u8, 0x00u8, 0u8, 0u8, 1u8, 0u8, 0u8, 0u8, 0u8];
     let (n1, val1) = <_ as Combinator<&[u8], Vec<u8>>>::parse(&msg2, data1.as_slice())?;
     Ok(())
+}
+
+fn opt_then() {
+    let c1 = (Tag::new(U8, 1u8), U8);
+    let c2 = (Tag::new(U8, 2u8), U16Le);
+    let c3 = (Tag::new(U8, 3u8), U32Le);
+    let c1 = OptThen::new(
+        c1,
+        OptThen::new(
+            c2,
+            OptThen::new(
+                c3,
+                End,
+            ),
+        ),
+    );
+    assert(c1.requires());
 }
 
 } // verus!
