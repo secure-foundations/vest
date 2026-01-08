e4a126baa325d0bc39b7dc9981915139abdfd793
y1ca1 <caiyeee@outlook.com>
2026-01-08 22:59:51 +0800

    Demonstrate pinpointing format malleability

--- vest++/src/tests/malleable.rs
+++ vest++/src/tests/malleable.rs
@@ -1,5 +1,5 @@
 use crate::combinators::refined::Tag;
-use crate::combinators::{Fixed, Preceded, Refined, Terminated};
+use crate::combinators::{BerBool, Fixed, Preceded, Refined, Terminated};
 use crate::core::{
     proof::{Deterministic, NonMalleable, PSRoundTrip, SPRoundTrip},
     spec::{SpecCombinator, SpecParser, SpecSerializer, SpecType},
@@ -136,6 +136,77 @@ proof fn test_double_terminated_tag_deterministic(v: Seq<u8>)
     assert(inner.1.wf(val1));
     assert(outer.1.wf(val2));
     requires_deterministic(outer, v, obuf);  // Should pass: both Tags have UniqueWfValue
+}
+
+// BerBool: Malleable Boolean Combinator Tests
+
+proof fn test_berbool_sp_roundtrip(v: bool, obuf: Seq<u8>)
+{
+    let combinator = BerBool;
+    requires_sp_roundtrip(combinator, v, obuf); // Should pass: BerBool implements SPRoundTrip
+}
+
+proof fn test_berbool_fails_ps_roundtrip(ibuf: Seq<u8>, obuf: Seq<u8>)
+{
+    let parser = BerBool;
+    // requires_ps_roundtrip(parser, ibuf, obuf); // Would fail: BerBool does not implement PSRoundTrip
+}
+
+proof fn test_berbool_fails_non_malleable(buf1: Seq<u8>, buf2: Seq<u8>)
+{
+    let parser = BerBool;
+    // requires_non_malleable(parser, buf1, buf2); // Would fail: BerBool does not implement NonMalleable
+}
+
+proof fn test_large_format_with_berbools()
+{
+    // Format: [Header 0xAA] [(BerBool, BerBool, Fixed::<2>)] [Footer 0xFF]
+    
+    let format = Terminated(
+        Preceded(
+            Tag { inner: Fixed::<1>, tag: seq![0xAAu8] },
+            ((BerBool, BerBool), Fixed::<2>)
+        ),
+        Tag { inner: Fixed::<1>, tag: seq![0xFFu8] }
+    );
+    
+    let v = ((true, false), seq![0x11u8, 0x22u8]);
+    let obuf = Seq::empty();
+    
+    // establish well-formedness of header and footer (for serializability)
+    let header_val = seq![0xAAu8];
+    let footer_val = seq![0xFFu8];
+    assert(format.0.0.wf(header_val));
+    assert(format.1.wf(footer_val));
+    
+    // serialize-parse roundtrip should still hold
+    requires_sp_roundtrip(format, v, obuf);
+    // requires_non_malleable(full_format, header_val, footer_val); // Should fail: BerBool is malleable
+    
+    // But multiple different buffers parse to the same value
+    // [0xAA] [0x01] [0x00] [0x11, 0x22] [0xFF] - true encoded as 0x01
+    // [0xAA] [0xFF] [0x00] [0x11, 0x22] [0xFF] - true encoded as 0xFF
+    // [0xAA] [0x42] [0x00] [0x11, 0x22] [0xFF] - true encoded as 0x42
+    
+    let buf1 = seq![0xAAu8, 0x01u8, 0x00u8, 0x11u8, 0x22u8, 0xFFu8];
+    let buf2 = seq![0xAAu8, 0xFFu8, 0x00u8, 0x11u8, 0x22u8, 0xFFu8];
+    let buf3 = seq![0xAAu8, 0x42u8, 0x00u8, 0x11u8, 0x22u8, 0xFFu8];
+    
+    if let Some((n1, v1)) = format.spec_parse(buf1) {
+        if let Some((n2, v2)) = format.spec_parse(buf2) {
+            if let Some((n3, v3)) = format.spec_parse(buf3) {
+                assert(v1 == v);
+                assert(v2 == v);
+                assert(v3 == v);
+                assert(n1 == n2 == n3);
+                
+                // But all byte prefixes are different
+                assert(buf1.take(n1)[1] == 0x01u8);
+                assert(buf2.take(n2)[1] == 0xFFu8);
+                assert(buf3.take(n3)[1] == 0x42u8);
+            }
+        }
+    }
 }
 
 } // verus!
