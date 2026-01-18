894c66387d6f5ed62684953a735fa4e4909b16a7
y1ca1 <caiyeee@outlook.com>
2026-01-18 16:48:14 +0800

    Add `fails_to_parse` helper

--- vest++/src/core/spec.rs
+++ vest++/src/core/spec.rs
@@ -279,6 +279,10 @@ pub trait SpecParser {
     /// from the input buffer `ibuf`, and `v` is the parsed value.
     /// Returns `None` if parsing fails.
     spec fn spec_parse(&self, ibuf: Seq<u8>) -> Option<(int, Self::PT)>;
+
+    open spec fn fails_to_parse(&self, ibuf: Seq<u8>) -> bool {
+        self.spec_parse(ibuf) is None
+    }
 }
 
 /// A well-behaved parser that satisfies key properties.
