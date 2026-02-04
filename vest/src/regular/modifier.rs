9dd62c2c4edb991ec00acdc448981081f5df5bb7
y1ca1 <caiyeee@outlook.com>
2026-02-03 20:52:31 -0500

    `FixedLen` now takes (owned | mutable reference) to the underlying len

--- vest/src/regular/modifier.rs
+++ vest/src/regular/modifier.rs
@@ -110,7 +110,7 @@ where
         self.inner.serialize(src, data, pos)
     }
 
-    fn generate(&self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
+    fn generate(&mut self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
         let (n, v) = self.inner.generate(g)?;
         Ok((n, self.mapper.forward_owned(v)))
     }
@@ -172,7 +172,7 @@ where
         self.inner.serialize(v, data, pos)
     }
 
-    fn generate(&self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
+    fn generate(&mut self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
         loop {
             let (n, v) = self.inner.generate(g)?;
             if (self.predicate)(Inner::SType::ref_to_stype(&v)) {
@@ -182,22 +182,62 @@ where
     }
 }
 
+/// Runtime value that can either own a value or borrow an external mutable location.
+#[allow(missing_docs)]
+#[derive(Debug, PartialEq, Eq)]
+pub enum RuntimeValue<'a, T> {
+    Value(T),
+    Ref(&'a mut T),
+}
+
+#[allow(missing_docs)]
+impl<'a, T> RuntimeValue<'a, T> {
+    pub fn from_value(v: T) -> Self {
+        RuntimeValue::Value(v)
+    }
+
+    pub fn from_mut(ptr: &'a mut T) -> Self {
+        RuntimeValue::Ref(ptr)
+    }
+
+    pub fn as_ref(&self) -> &T {
+        match self {
+            RuntimeValue::Value(v) => v,
+            RuntimeValue::Ref(v) => &**v,
+        }
+    }
+
+    pub fn as_mut(&mut self) -> &mut T {
+        match self {
+            RuntimeValue::Value(v) => v,
+            RuntimeValue::Ref(v) => &mut **v,
+        }
+    }
+}
+
+#[allow(missing_docs)]
+impl<'a> RuntimeValue<'a, u16> {
+    pub fn from_u16_mut(ptr: &'a mut u16) -> Self {
+        RuntimeValue::Ref(ptr)
+    }
+}
+
 /// Combinator that runs its inner combinator only when `lhs` equals `rhs`.
-pub struct CondEq<T, Inner> {
+pub struct CondEq<'a, T, Inner> {
     /// The left-hand side of the equality comparison.
-    pub lhs: T,
+    pub lhs: RuntimeValue<'a, T>,
     /// The right-hand side of the equality comparison.
     pub rhs: T,
     /// The conditional combinator.
     pub inner: Inner,
 }
 
-impl<I, O, T, Inner> Combinator<I, O> for CondEq<T, Inner>
+impl<'a, I, O, T, Inner> Combinator<I, O> for CondEq<'a, T, Inner>
 where
     I: VestInput + ?Sized,
     O: VestOutput<I>,
     Inner: Combinator<I, O>,
-    T: PartialEq,
+    T: PartialEq + Copy,
 {
     type Type<'p>
         = Inner::Type<'p>
@@ -220,7 +260,7 @@ where
     where
         I: 'p,
     {
-        if self.lhs == self.rhs {
+        if *self.lhs.as_ref() == self.rhs {
             self.inner.parse(s)
         } else {
             Err(ParseError::CondFailed)
@@ -236,19 +276,12 @@ where
     where
         I: 's,
     {
-        if self.lhs == self.rhs {
-            self.inner.serialize(v, data, pos)
-        } else {
-            Err(SerializeError::Other("condition not satisfied".into()))
-        }
+        self.inner.serialize(v, data, pos)
     }
 
-    fn generate(&self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
-        if self.lhs == self.rhs {
-            self.inner.generate(g)
-        } else {
-            Err(GenerateError::CondFailed)
-        }
+    fn generate(&mut self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
+        *self.lhs.as_mut() = self.rhs;
+        self.inner.generate(g)
     }
 }
 
@@ -303,15 +336,69 @@ where
         self.1.serialize(v, data, pos)
     }
 
-    fn generate(&self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
+    fn generate(&mut self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
         self.1.generate(g)
     }
 }
 
-/// Combinator that makes sure `Inner` parses/serializes exactly `n` bytes.
-pub struct FixedLen<Inner>(pub usize, pub Inner);
+/// Length value that may live internally or as an external mutable reference
+#[derive(Debug, PartialEq, Eq)]
+#[allow(missing_docs)]
+pub enum Length<'a> {
+    Value(usize),
+    RefU8(&'a mut u8),
+    RefU16(&'a mut u16),
+    RefU32(&'a mut u32),
+    RefU64(&'a mut u64),
+}
 
-impl<I, O, Inner> Combinator<I, O> for FixedLen<Inner>
+#[allow(missing_docs)]
+impl<'a> Length<'a> {
+    pub fn from_value(v: usize) -> Self {
+        Length::Value(v)
+    }
+
+    pub fn from_u8_mut(ptr: &'a mut u8) -> Self {
+        Length::RefU8(ptr)
+    }
+
+    pub fn from_u16_mut(ptr: &'a mut u16) -> Self {
+        Length::RefU16(ptr)
+    }
+
+    pub fn from_u32_mut(ptr: &'a mut u32) -> Self {
+        Length::RefU32(ptr)
+    }
+
+    pub fn from_u64_mut(ptr: &'a mut u64) -> Self {
+        Length::RefU64(ptr)
+    }
+
+    pub fn get(&self) -> usize {
+        match self {
+            Length::Value(v) => *v,
+            Length::RefU8(p) => **p as usize,
+            Length::RefU16(p) => **p as usize,
+            Length::RefU32(p) => **p as usize,
+            Length::RefU64(p) => **p as usize,
+        }
+    }
+
+    pub fn set(&mut self, v: usize) {
+        match self {
+            Length::Value(val) => *val = v,
+            Length::RefU8(p) => **p = v as u8,
+            Length::RefU16(p) => **p = v as u16,
+            Length::RefU32(p) => **p = v as u32,
+            Length::RefU64(p) => **p = v as u64,
+        }
+    }
+}
+
+/// Combinator that makes sure `Inner` parses/serializes exactly `n` bytes.
+pub struct FixedLen<'a, Inner>(pub Length<'a>, pub Inner);
+
+impl<'a, I, O, Inner> Combinator<I, O> for FixedLen<'a, Inner>
 where
     I: VestInput + ?Sized,
     O: VestOutput<I>,
@@ -327,23 +414,23 @@ where
         I: 's;
     type GType = Inner::GType;
 
-    fn length<'s>(&self, _v: Self::SType<'s>) -> usize
+    fn length<'s>(&self, v: Self::SType<'s>) -> usize
     where
         I: 's,
     {
-        self.0
+        self.1.length(v)
     }
 
     fn parse<'p>(&self, s: &'p I) -> Result<(usize, Self::Type<'p>), ParseError>
     where
         I: 'p,
     {
-        let (n, chunk) = <_ as Combinator<I, O>>::parse(&Variable(self.0), s)?;
+        let (n, chunk) = <_ as Combinator<I, O>>::parse(&Variable(self.0.get()), s)?;
         let (m, value) = self.1.parse(&chunk)?;
         if m == n {
             Ok((n, value))
         } else {
-            Err(ParseError::AndThenUnusedBytes)
+            Err(ParseError::FixedLenViolation)
         }
     }
 
@@ -359,7 +446,9 @@ where
         self.1.serialize(v, data, pos)
     }
 
-    fn generate(&self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
-        self.1.generate(g)
+    fn generate(&mut self, g: &mut GenSt) -> GResult<Self::GType, GenerateError> {
+        let (n, v) = self.1.generate(g)?;
+        self.0.set(n);
+        Ok((n, v))
     }
 }
